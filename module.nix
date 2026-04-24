5ee617c8b80e3651c354fdda69391b591fd84cc7
Simon Kennedy <sffjunkie+code@gmail.com>
2026-04-24 18:39:04 +0100

    change: copy module from siteconfig

--- module.nix
+++ module.nix
@@ -1,26 +1,38 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }:
 let
-  cfg = config.looniversity.window_manager.qtile;
+  cfg = config.looniversity.desktop.window_manager.lwm;
 
-  inherit (pkgs.lib)
+  inherit (lib)
     mkEnableOption
-    mkForce
     mkIf
     mkOption
     types
     ;
 in
 {
-  options.looniversity.window_manager.qtile = {
-    enable = mkEnableOption "Looniversity QTile";
+  options.looniversity.desktop.window_manager.lwm = {
+    enable = mkEnableOption "qtile";
+    configPath = mkOption {
+      type = types.str;
+      default = "lwm";
+    };
+
+    dev = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Use the devleopment version of lwm";
+    };
+
     python = mkOption {
       type = types.package;
       default = pkgs.python3;
     };
+
     extraPythonPackages = mkOption {
       type = types.functionTo (types.listOf types.package);
       description = ''
@@ -28,6 +40,7 @@ in
         to be added to the default packages required by qtile.
       '';
     };
+
     environmentFile = mkOption {
       type = types.nullOr types.path;
       default = null;
@@ -38,42 +51,37 @@ in
   };
 
   config = mkIf cfg.enable {
+    looniversity.wayland.enable = true;
+    looniversity.desktop.window_manager.lwm.dev = true;
+
     environment.sessionVariables = {
-      MOZ_ENABLE_WAYLAND = "1";
       XDG_SESSION_TYPE = "wayland";
-      XDG_CURRENT_DESKTOP = "qtile";
-      SDL_VIDEODRIVER = "wayland,x11,windows";
-      QT_QPA_PLATFORM = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      _JAVA_AWT_WM_NONREPARENTING = "1";
+      XDG_CURRENT_DESKTOP = "lwm";
     };
 
-    systemd.user.services.qtile =
+    systemd.user.services.lwm =
       let
         pyEnv = cfg.python.withPackages (
           ps:
           [
             ps.qtile
-            ps.iwlib
           ]
           ++ (cfg.extraPythonPackages ps)
         );
       in
       {
-        description = "Qtile - Wayland window manager";
+        description = "lwm - Looniversity window manager";
         documentation = [ "man:qtile(5)" ];
-        bindsTo = [ "graphical-session.target" ];
-        wants = [ "graphical-session-pre.target" ];
-        after = [ "graphical-session-pre.target" ];
+        bindsTo = [ "lde-session.target" ];
+        wants = [ "lde-session.target" ];
+        after = [ "lde-session.target" ];
 
-        # We explicitly unset PATH here, as we want it to be set by
-        # systemctl --user import-environment in startqtile
-        environment.PATH = mkForce null;
-        environment.PYTHONPATH = mkForce null;
+        environment.PATH = lib.mkForce null;
+        environment.PYTHONPATH = lib.mkForce null;
 
         serviceConfig = {
           Type = "simple";
-          ExecStart = "${pyEnv}/bin/qtile start --backend wayland --config ${pkgs.qtileconfig}";
+          ExecStart = "${pyEnv}/bin/qtile start --backend wayland --config %h/.config/${config.looniversity.desktop.window_manager.lwm.configPath}";
           Restart = "on-failure";
           RestartSec = 1;
           TimeoutStopSec = 10;

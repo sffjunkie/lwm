49318c22cc637d7258bc51f1e095fea8cbc9dc20
Simon Kennedy <sffjunkie+code@gmail.com>
2026-05-01 13:38:27 +0100

    change: build the floating defnitions from the config

--- src/lwm/builder/floating.py
+++ src/lwm/builder/floating.py
@@ -5,15 +5,15 @@ from qtile_extras.layout.decorations.borders import RoundedCorners
 from lwm.loader.typedef import Config
 
 
-def float_rules() -> list[_Match]:
+def float_rules(config: Config) -> list[_Match]:
     return [
-        Match(wm_class=float_match) for float_match in wmclass_float
+        Match(wm_class=float_match) for float_match in config.floating.match["wm_appid"]
     ] + layout.Floating.default_float_rules
 
 
 def build_floating(config: Config) -> layout.Floating:
     return layout.Floating(
-        float_rules=float_rules(),
+        float_rules=float_rules(config),
         border_width=config.layout.common.border_width,
         border_normal=RoundedCorners(
             colour=config.color.named.floating_border_normal,

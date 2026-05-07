e36b1d807053c05e6607b288e997c455437f77d6
Simon Kennedy <sffjunkie+code@gmail.com>
2026-05-07 16:04:14 +0100

    refactor: use updated controller model

--- src/lwm/loader/controller/loader.py
+++ src/lwm/loader/controller/loader.py
@@ -2,9 +2,6 @@ from lwm.loader.controller.model import ControllerDefs
 
 
 def controllerdefs_from_data(data: dict):
-    controller_data = data.get("controller", None)
-    if controller_data is None:
-        controllers = ControllerDefs()
-    else:
-        controllers = ControllerDefs(**controller_data)
+    controller_data = data.get("controller", {})
+    controllers = ControllerDefs(**controller_data)
     return controllers

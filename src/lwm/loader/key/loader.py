5b79ddeb9c78185aece3c89a58ae2fe110cc0480
Simon Kennedy <sffjunkie+code@gmail.com>
2026-05-07 16:09:54 +0100

    refactor: simplify code

--- src/lwm/loader/key/loader.py
+++ src/lwm/loader/key/loader.py
@@ -1,12 +1,10 @@
-from lwm.loader.key.model import KeyDefs, KeyMapping
+from libqtile.lazy import lazy
+
+from lwm.loader.key.model import KeyDefs
 
 
 def keydefs_from_data(data: dict) -> KeyDefs:
-    key_data = data.get("key", None)
-    if key_data is None:
-        mapping = KeyMapping()
-        keys = KeyDefs(mapping=mapping, defs=[])
-    else:
-        keys = KeyDefs(**key_data)
+    key_data = data.get("key", {})
+    keys = KeyDefs(**key_data)
 
     return keys

e6113956e3d26480201e3ffbca142a981e7b64ff
Simon Kennedy <sffjunkie+code@gmail.com>
2026-05-25 18:29:08 +0100

    fix: return layout class not option dictionary

--- src/lwm/builder/layout.py
+++ src/lwm/builder/layout.py
@@ -1,7 +1,29 @@
 from libqtile.layout.base import _SimpleLayoutBase
+from libqtile import layout
 from qtile_extras.layout.decorations.borders import RoundedCorners
 
 from lwm.model.definitions import Definitions
+
+_layouts = {
+    "bsp": layout.Bsp,
+    "columns": layout.Columns,
+    "floating": layout.Floating,
+    "matrix": layout.Matrix,
+    "max": layout.Max,
+    "plasma": layout.Plasma,
+    "ratiotile": layout.RatioTile,
+    "screensplit": layout.ScreenSplit,
+    "slice": layout.Slice,
+    "spiral": layout.Spiral,
+    "stack": layout.Stack,
+    "tile": layout.Tile,
+    "treetab": layout.TreeTab,
+    "verticaltile": layout.VerticalTile,
+    "monadtall": layout.MonadTall,
+    "monadthreecol": layout.MonadThreeCol,
+    "monadwide": layout.MonadWide,
+    "zoomy": layout.Zoomy,
+}
 
 
 def _layout_type_args(defs: Definitions, layout: str) -> dict:
@@ -23,6 +45,6 @@ def build_layouts(defs: Definitions) -> list[_SimpleLayoutBase]:
     layouts = []
     for layout_name in defs.layout.default_layouts:
         d = dict(getattr(defs.layout.layouts, layout_name)) | base_args
-        layouts.append(d)
+        layouts.append(_layouts[layout_name](**d))
 
     return layouts

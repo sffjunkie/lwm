a362f0897a3731d37d48d7a4004620d969811817
Simon Kennedy <sffjunkie+code@gmail.com>
2026-05-27 17:04:38 +0100

    change: remove systemctl output, change to warning level

--- src/lwm/runtime_info.py
+++ src/lwm/runtime_info.py
@@ -2,17 +2,13 @@ import sys
 
 from libqtile import __path__ as libqtile_path
 from libqtile.log_utils import logger
-from lwm.helper.shellcmd import shellcmd_output
 
 
 def log_runtime_info() -> None:
-    logger.debug(f"python prefix: {sys.prefix}")
-    logger.debug(f"python version: {sys.version}")
-    logger.debug(f"python platform: {sys.platform}")
-    logger.debug(f"python path: {sys.path}")
-    logger.debug(f"libqtile path: {libqtile_path}")
-    logger.debug(f"__init__ file: {__file__}")
-    logger.debug(f"__package__: {__package__}")
-
-    lwm_env = shellcmd_output(["systemctl", "--user", "show-environment"])
-    logger.debug(f"lwm env: {lwm_env}")
+    logger.warning(f"python prefix: {sys.prefix}")
+    logger.warning(f"python version: {sys.version}")
+    logger.warning(f"python platform: {sys.platform}")
+    logger.warning(f"python path: {sys.path}")
+    logger.warning(f"libqtile path: {libqtile_path}")
+    logger.warning(f"__init__ file: {__file__}")
+    logger.warning(f"__package__: {__package__}")
